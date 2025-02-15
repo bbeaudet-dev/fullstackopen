4379eb7f84e8293adce1e45cfe85daf15bd6e07f
bbeaudet-dev <bbeaudet0@gmail.com>
2025-02-15 00:24:45 -0500

    Experimented with gambling app using buttons and counters

--- part1/counter/src/App.jsx
+++ part1/counter/src/App.jsx
@@ -1,17 +1,46 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value',counter)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const decreaseByOne = () => {
+    setCounter(counter - 1)
+    console.log('decreasing, value before', counter)
+  }
 
-  console.log('rendering...', counter)
-  
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+    console.log('increasing, value before', counter)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
 
