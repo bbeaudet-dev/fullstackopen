e2e98d43be2b813faa7b585d244f72746436078c
Ben Beaudet <bbeaudet0@gmail.com>
2025-05-09 17:20:50 -0400

    Transferred files

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = UseState([])
+
+  const handleGood = () => {
+
+  }
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
---
+++ testfiles/test.jsx
@@ -0,0 +1,29 @@
+const sum = (n1, n2) => {
+    console.log(n1, n2)
+    return n1 + n2
+}
+const result = sum(5,7)
+console.log(result)
+
+const Pikachu = {
+    id: 1,
+    type1: 'electric',
+    type2: 'none'
+    HP: 200,
+    Atk: 50,
+    SpAtk: 150,
+    Def: 75,
+    SpDef: 75,
+    Speed: 125,
+}
+const Zapdos = {
+    id: 2,
+    type1: 'electric',
+    type2: 'flying',
+    HP: 300,
+    Atk: 80,
+    SpAtk: 225,
+    Def: 90,
+    SpDef: 100,
+    Speed: 150,
+}
